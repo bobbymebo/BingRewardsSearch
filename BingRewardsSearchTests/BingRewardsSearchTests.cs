6725f9a8e3bb852fde8050c6ec7fd0e5103ac2e2
Bobby Borisov <bobby.borisov@ymail.com>
2017-08-01 09:04:42 -0700

    Initial Check-in

--- BingRewardsSearchTests/BingRewardsSearchTests.cs
+++ BingRewardsSearchTests/BingRewardsSearchTests.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BingRewardsSearch
+namespace BingRewardsSearchTests
 {
-    public class Class1
+    public class BingRewardsSearchTests
     {
     }
 }
